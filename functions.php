b3a9e99230b76f2e2da8ac0bce2accef635afc5b
mrksye <mrksye@gmail.com>
2022-01-26 14:40:21 +0900

    修正: AJAXのpost_mailの構造変更

--- functions.php
+++ functions.php
@@ -61,48 +61,5 @@ add_action('wp_enqueue_scripts', function () {
 	wp_enqueue_script('animate', get_template_directory_uri() . '/js/animate.js', [], '', true);
 } );
 
-
-add_action( 'wp_head', function () {
-?>
-    <script>
-        var MAILFORMURL = '<?php echo admin_url( 'admin-ajax.php'); ?>';
-    </script>
-<?php
-}, 1 );
-
-function send_mail(){
-
-  $missionCategory = $_POST["missionCategory"];
-  $missionUrl = $_POST["missionUrl"];
-  $uses = $_POST["uses"];
-  $customerName = $_POST["customerName"];
-  $companyName = $_POST["companyName"];
-  $phone = $_POST["phone"];
-  $email = $_POST["email"];
-  $provincial = $_POST["provincial"];
-  $message = $_POST["message"];
-
-
-  $blogname = get_option( 'blogname' );
-  $headers = array( 'Content-Type: text/plain; charset=UTF-8','From: me Myself ' );
-  $subject = $blogname.'の問い合わせ';
-  $to = get_option('admin_email');$body = "Web制作補助金サポートへのお問い合わせです"."\n"
-	  ."\n"
-	  ."相談項目:".$missionCategory."\n"
-	  ."相談サイト:".$missionUrl."\n"
-	  ."補助金用途:".$uses."\n"
-	  ."名前:".$customerName."\n"
-	  ."会社名:".$companyName."\n"
-	  ."メール:".$email."\n"
-	  ."電話:".$phone."\n"
-	  ."都道府県:".$provincial."\n"."内容".$message."\n";
-
-  wp_mail( $to, $subject, $body, $headers );
-    // echoで、クライアント側に返すデータを送信する
-  echo $result;
- 
-    // dieしておかないと末尾に余計なデータ「0」が付与されるので注意
-  die();
-}
-add_action( 'wp_ajax_send_mail', 'send_mail' );
-add_action( 'wp_ajax_nopriv_send_mail', 'send_mail' );
+//AJAXでwp_mailを起動させるスクリプトを読み込む
+include_once("./inc/post_mail.php");
--- inc/post-mail.php
+++ inc/post-mail.php
@@ -1,10 +1,17 @@
 <?php
-  require('../../../../wp-blog-header.php');
-  require_once("../../../../wp-config.php");
 
-  // chenge html mail
+add_action( 'wp_head', function () {
+?>
+    <script>
+        var MAILFORMURL = '<?php echo admin_url( 'admin-ajax.php'); ?>';
+    </script>
+<?php
+}, 1 );
+
+function send_mail(){
+
   $missionCategory = $_POST["missionCategory"];
-  $missionUrl = $_POST["missionUrl"];
+  $missionUrl = $_POST["missionUrl"];
   $uses = $_POST["uses"];
   $customerName = $_POST["customerName"];
   $companyName = $_POST["companyName"];
@@ -16,19 +23,26 @@
 
   $blogname = get_option( 'blogname' );
   $headers = array( 'Content-Type: text/plain; charset=UTF-8','From: me Myself ' );
-  $subject = $blogname.'の問い合わせ';
-  $to = "bestlifenamba0000@gmail.com";//get_option('admin_email');
-  $body = "Web制作補助金サポートへのお問い合わせです"."\n"
+  $subject = $blogname.'の問い合わせ';
+  $to = get_option('admin_email');$body = "Web制作補助金サポートへのお問い合わせです"."\n"
 	  ."\n"
-	  ."相談項目:".$missionCategory."\n"
+	  ."相談項目:".$missionCategory."\n"
 	  ."相談サイト:".$missionUrl."\n"
 	  ."補助金用途:".$uses."\n"
 	  ."名前:".$customerName."\n"
 	  ."会社名:".$companyName."\n"
 	  ."メール:".$email."\n"
 	  ."電話:".$phone."\n"
-	  ."都道府県:".$provincial."\n"
-	  ."内容".$message."\n";
+	  ."都道府県:".$provincial."\n"."内容".$message."\n";
 
   wp_mail( $to, $subject, $body, $headers );
-?>
+    // echoで、クライアント側に返すデータを送信する
+  $data = ['result' => 'SUCCESS'];
+  header("Content-Type: application/json; charset=utf-8");
+  echo json_encode($data);
+ 
+    // dieしておかないと末尾に余計なデータ「0」が付与されるので注意
+  die();
+}
+add_action( 'wp_ajax_send_mail', 'send_mail' );
+add_action( 'wp_ajax_nopriv_send_mail', 'send_mail' );
