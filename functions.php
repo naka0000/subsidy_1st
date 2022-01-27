01e9c3c12ca9c5ad5f758564d487cc3359ce6963
mrksye <mrksye@gmail.com>
2022-01-27 18:33:31 +0900

    改名: コメントの改名
    functions.php内のコメントを細かく記載。

--- functions.php
+++ functions.php
@@ -8,12 +8,23 @@ add_theme_support('post-thumbnails');
 //<p>タグを付けない
 remove_filter('the_content','wpautop');
 
-//wordpressの様々な機能をwp_head関数から取り外す
+//RSSフィードは、サイトがブログ的な性格を帯びている場合は必要ですが、それ以外の場合はとくに必要ないので削除
+remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
+
+//RSD(Really Simple Discovery)用のxmlへのリンクは、WordPress外部のサービスを使用する際、そのサービスがWordPress内の情報を取得するために必要とされるものです。外部サービスを使ってサイトを運営する予定がないので削除
 remove_action('wp_head', 'rsd_link');
+
+//Microsoftが提供するブログエディター「Windows Live Writer」は使用する予定がないので削除
 remove_action('wp_head', 'wlwmanifest_link');
+
+//前後のページへのリンクを出力するタグはブログじゃないし使わないので削除
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+
+//wordpressを使ってサイトを作成したことを公にするタグはWordPressのバージョン情報がバレてよろしくないので削除
 remove_action('wp_head', 'wp_generator');
+
+//WordPress4.4までは、投稿画面に[短縮URLを取得]ボタンが表示されていましたが、4.5以降では表示されなくなりましたがヘッドに情報が残っているので削除
 remove_action('wp_head', 'wp_shortlink_wp_head');
 
 //絵文字の表示をwp_head関数などから削除
@@ -26,13 +37,11 @@ add_action( 'init', function () {
      remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );    
      remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 } );
-
 //インラインスタイル削除(html直書きstylesheet削除)
 add_action( 'widgets_init', function (){
 	global $wp_widget_factory;
 	remove_action('wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ));
 } );
-
 
 //初期のCSSとプラグインのCSSをwp_head関数から剥がす
 add_action( 'wp_enqueue_scripts', function () {
@@ -62,5 +71,7 @@ add_action('wp_enqueue_scripts', function () {
 	wp_enqueue_script('post_mail', get_template_directory_uri() . '/js/post_mail.js', [], '', true);
 } );
 
+
+//ここからは独自PHPの読み込み。
 //AJAXでwp_mailを起動させるスクリプトを読み込む
 require_once("inc/post_mail.php");
