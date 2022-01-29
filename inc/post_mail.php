c6d070e02460b7e1c9c22209027317c2635ef632
naka0000 <bestlifenamba0000@gmail.com>
2022-01-30 01:18:12 +0900

    追加: お客様への自動返信メールの追加

--- inc/post_mail.php
+++ inc/post_mail.php
@@ -13,7 +13,7 @@ function send_mail(){
     $json = file_get_contents("php://input");
     // JSON文字列をobjectに変換 ⇒ 第2引数をtrueにしないとハマるので注意
     $formDatas = json_decode($json, true);
-
+    
     $blogname = get_option( 'blogname' );
     $headers = array( 'Content-Type: text/plain; charset=UTF-8','From: me Myself ' );
     $subject = $blogname.'の問い合わせ';
@@ -32,10 +32,35 @@ function send_mail(){
 
     //メール送信をする。
     $sendingDone = wp_mail( $to, $subject, $body, $headers );
+
+    //お客さんにも自動返信を行う。
+    $customerAddress = $formDatas["emeil"];
+    $autoReplySubject = "GRIT GROUPのWeb制作補助金サポートへのお問い合わせが完了しました。";
+    $autoReplyBody = $formDatas["customerName"]."様"
+                    ."この度はGRIT GROUPのWeb制作補助金サポートへお問い合わせいただきありがとうございます"."\n"
+                    ."\n"
+                    ."以下のお問い合わせ内容を受け付けました。"."\n"
+                    ."\n"
+                    ."\n"
+                    ."相談項目: ".$formDatas["missionCategory"]."\n"
+                    ."相談サイト: ".$formDatas["missionUrl"]."\n"
+                    ."補助金用途: ".implode(' / ', $formDatas["uses"])."\n"
+                    ."名前: ".$formDatas["customerName"]."\n"
+                    ."会社名: ".$formDatas["companyName"]."\n"
+                    ."電話: ".$formDatas["phone"]."\n"
+                    ."メール: ".$formDatas["email"]."\n"
+                    ."都道府県: ".$formDatas["provincial"]."\n"
+                    ."内容: ".$formDatas["message"]."\n"
+                    ."\n"
+                    ."3営業日以内に当社担当からご連絡を差し上げます。"
+                    ."今しばらくお待ちください";
+    $headers = array( 'Content-Type: text/plain; charset=UTF-8','From: 株式会社GRIT GROUP' );
+    wp_mail( $customerAddress, $autoReplySubject, $autoReplyBody, $headers );
+
     // 送信完了したことをフロントエンドに返す。エラーがあればサーバーエラーであったことを返す。
     header("Content-Type: application/json; charset=utf-8");
     if($sendingDone){
-        echo '<p>以下の内容で送信が完了しました。<br>ご連絡をお待ちください。</p>';
+        echo '<p>以下の内容で送信が完了しました。<br>自動返信メールが届いていることをご確認の上、ご連絡お待ちください。</p>';
     } else {
         echo '<p>サーバー処理でエラーが発生しました。<br>お電話でのお問い合わせをお願いします。</p>';
     }
