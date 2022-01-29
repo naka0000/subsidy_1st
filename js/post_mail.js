352b48af9ac767f1ed5a71e48c725e5f2473998a
naka0000 <bestlifenamba0000@gmail.com>
2022-01-29 20:23:37 +0900

    Revert "修正: fitstElementChildからfirstChildへ変更"
    This reverts commit 44f91f6a199a0fafb1ccf71243ea32b958105653.

--- js/post_mail.js
+++ js/post_mail.js
@@ -178,7 +178,7 @@ function insertConfirmText() {
 								</div>`;
 
 	//HTMLElementに変換(appendChild関数のクラスに合わせるため。adjacentHTML関数でそのまま文字列を差し込むのはエラーの温床になるため。)			
-	let confirmForm = new DOMParser().parseFromString(stringConfirmForm, "text/html").querySelector("html body").firstChild;
+	let confirmForm = new DOMParser().parseFromString(stringConfirmForm, "text/html").querySelector("html body").firstElementChild;
 
 	//入力フォームを非表示にして確認フォームを差し込む。ユーザーに送信準備ができたデータを表示するため。
 	document.getElementById("ajaxForm").setAttribute("style", "display:none;");
@@ -257,7 +257,7 @@ function autoDisappearModal (message, timeout) {
 									</div>
 								</div>
 							</div>`
-	let modalDialog = new DOMParser().parseFromString(stringModalDialog, "text/html").querySelector("html body").firstChild;
+	let modalDialog = new DOMParser().parseFromString(stringModalDialog, "text/html").querySelector("html body").firstElementChild;
 	document.querySelector('html body').appendChild(modalDialog);
 	let modal = document.getElementById('modalBox');
 	fadeIn(modal, 200);
